b21f8f0c1fd3b242b739b9ed5ffde77e3f7682ba
jainprachi25 <jainprachi.55@gmail.com>
2015-09-25 15:57:28 -0700

    added light dataset

--- src/main/java/demo/LightDO.java
+++ src/main/java/demo/LightDO.java
@@ -1,7 +1,109 @@
 package demo;
 
-/**
- * Created by prachijain on 9/25/15.
- */
-public class LightDO {
-}
+    import java.util.Date;
+
+    public class LightDO {
+
+        // Light Status
+        private int id;
+        private String name;
+        private String modelid;
+        private boolean on;
+        private int bri ;
+        private int hue;
+        private int sat;
+        private boolean reachable;
+        private String type;
+
+        // Light Config
+        private Date last_use_date;
+        private Date created_date;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getDevice_id() {
+            return id;
+        }
+
+        public void setDevice_id(int id) {
+            this.id = id;
+        }
+
+        public String getModel_id() {
+            return modelid;
+        }
+
+        public void setModel_id(String modelid) {
+            this.modelid = modelid;
+        }
+
+        public boolean getOnStatus() {
+            return on;
+        }
+
+        public void setOnStatus(Boolean on) {
+            this.on = on;
+        }
+
+        public int getBrightness() {
+            return bri;
+        }
+
+        public void setBrightness(int bri) {
+            this.bri = bri;
+        }
+
+        public int getSaturation() {
+            return sat;
+        }
+
+        public void setSaturation(int sat) {
+            this.sat = sat;
+        }
+
+        public int getHueColor() {
+            return hue;
+        }
+
+        public void setHueColor(int hue) {
+            this.hue = hue;
+        }
+
+        public Boolean getReachableStatus() {
+            return reachable;
+        }
+
+        public void setReachableStatus(Boolean reachable) {
+            this.reachable = reachable;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public Date getLast_use_date(){
+            return last_use_date;
+        }
+
+        public void setLast_use_date(Date last_use_date){
+            this.last_use_date = last_use_date;
+        }
+
+        public Date getCreated_date(){
+            return created_date;
+        }
+
+        public void setCreated_date(Date created_date){
+            this.created_date = created_date;
+        }
+    }
